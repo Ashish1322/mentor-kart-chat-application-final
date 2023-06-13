7e15298fe18e445d6cce0302fb277ead7c234929
Ashish1322 <69026067+Ashish1322@users.noreply.github.com>
2023-06-13 19:49:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import { getAuth } from "firebase/auth";
 import {getDatabase} from "firebase/database"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDP__7lqy9aBGjo2HVxgHjWhEX69PhsVQ8",
-  authDomain: "temp-project-5779c.firebaseapp.com",
-  databaseURL: "https://temp-project-5779c-default-rtdb.firebaseio.com",
-  projectId: "temp-project-5779c",
-  storageBucket: "temp-project-5779c.appspot.com",
-  messagingSenderId: "766017024852",
-  appId: "1:766017024852:web:0c8e35d652f53f63f88ec1"
+ // your configurations
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
